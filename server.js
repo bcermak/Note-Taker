827c8a0d34f0d8a7e750d4914dd86361ed27c4a2
Brandon Cermak <cermakprofessional@gmail.com>
2020-11-18 19:58:20 -0600

    fixed server issue

--- server.js
+++ server.js
@@ -3,30 +3,28 @@ const path = require("path");
 const fs = require('fs');
 const app = express();
 
-const PORT = 8082;
+var PORT = process.env.PORT || 8080;
 
 function handleRequest(request, response) {
-
-    // Send the below string to the client when the user visits the PORT URL
     response.end("It Works!! Path Hit: " + request.url);
   }
-  
-  // Use the Node HTTP package to create our server.
-  // Pass the handleRequest function to empower it with functionality.
-  const server = http.createServer(handleRequest);
+
 
 //begin express
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 //get notes
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
 
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 })
+
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
 
 //turn on PORT
 app.listen(PORT, function() {
